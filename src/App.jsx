5d1e7abb89f42a857e810282230ea2ed5d2d80b5
karlo2021 <kbakot00@fesb.hr>
2022-04-05 17:05:45 +0200

    jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,83 @@
-
-class IssueFilter extends React.Component{
-    render(){
-        return(
-            <div>This is placeholder for issue filter.</div>
-        );
-    }
-}
-class IssueRow extends React.Component{
-  render()
+const issues = [{
+    id: 1, status: 'New', owner: 'Ravan', effort: 5,
+    created: new Date('2022-03-24'), due: undefined,
+    title: 'Error in console when clicking Add'
+  },
   {
-    const style = this.props.rowStyle;
-    return(
+    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2022-04-03'), due: new Date('2022-04-17'),
+    title: 'Missing bottom border on panel'
+  }
+];
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
       <tr>
-        <td style={style}>{this.props.issue_id}</td>
-        <td style={style}>{this.props.children}</td>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ''}</td>
+        <td>{issue.title}</td>
       </tr>
     );
   }
 }
 class IssueTable extends React.Component{
-    render(){
-        const rowStyle = {border: "1px solid silver", padding: 4};  
-        return(
-            <table style={{borderCollapse: "collapse"}}>
-              <thead>
-                <tr>
-                  <th style={rowStyle}>ID</th>
-                  <th style={rowStyle}>Title</th>
-                </tr>
-              </thead>
-              <tbody>
-                <IssueRow rowStyle={rowStyle} issue_id={1}>
-                  Error in console when clicking Add
-                </IssueRow>
-                <IssueRow rowStyle={rowStyle} issue_id={2}>
-                  <div>Missing <b>bottom</b> border on panel</div>
-                </IssueRow>
-              </tbody>
-            </table>
-        );
-    }
+  render() {
+    const issueRows = issues.map(issue =>
+      <IssueRow key={issue.id} issue={issue} />
+    );
+
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {issueRows}
+        </tbody>
+      </table>
+    );
+  }
 }
-class IssueAdd extends React.Component{
-    render(){
-        return(
-            <div>This is placeholder for a form to add an issue.</div>
-        );
-    }
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a form to add an issue.</div>
+    );
+  }
 }
-class IssueList extends React.Component{
-    render()
-    {
-        return(
-            <React.Fragment>
-                <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable />
-                <hr />
-                <IssueAdd />
-            </React.Fragment>
-        );
-    }
+class IssueList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
+    );
+  }
 }
 
 const element = <IssueList />
